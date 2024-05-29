ae67f90f7a9614d2fe1a9a8e07f90aae2bad0904
Vladimir <nachev.vlad@gmail.com>
2024-05-29 17:05:11 +0300

    adjustments

--- src/test/java/com/example/aliens_vs_humans/HumanTest.java
+++ src/test/java/com/example/aliens_vs_humans/HumanTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class HumanTest {
     String humanName = "John";
     int defaultHealth = 100;
